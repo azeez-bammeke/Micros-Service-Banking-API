657114cf7b0dc2b5782874a1d7016bcabac2894f
abammeke <abammeke222@gmail.com>
2024-01-06 16:24:21 -0600

    Added Gatewayserver

---
+++ GatewayServer/gatewayserver/gatewayserver/src/main/java/com/webnet/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,41 @@
+package com.webnet.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator webnetBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(appRoute -> appRoute.path("/webnet/accounts/**")
+                        .filters(f -> f.rewritePath("/webnet/accounts/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .addResponseHeader("X-Webnetnet-client-id", UUID.randomUUID().toString())
+                        )
+                        .uri("lb://ACCOUNTS")).route(appRoute -> appRoute.path("/webnet/loans/**")
+                        .filters(f -> f.rewritePath("/webnet/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .addResponseHeader("X-Webnetnet-client-id", UUID.randomUUID().toString())
+                        )
+                        .uri("lb://LOANS")).route(appRoute -> appRoute.path("/webnet/cards/**")
+                        .filters(f -> f.rewritePath("/webnet/cards/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .addResponseHeader("X-Webnetnet-client-id", UUID.randomUUID().toString())
+                        )
+                        .uri("lb://CARDS"))
+                .build();
+    }
+
+}
