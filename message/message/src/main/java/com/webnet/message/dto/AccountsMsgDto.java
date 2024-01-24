92cd513fe5f1efba51ac48d4c08c86b16d7b3b1c
abammeke <abammeke222@gmail.com>
2024-01-24 13:24:44 -0600

    [S13-201] - Using spring cloud function to build Rest Api with functions

---
+++ message/message/src/main/java/com/webnet/message/dto/AccountsMsgDto.java
@@ -0,0 +1,13 @@
+package com.webnet.message.dto;
+
+/*
+ *@param accountNumber
+ * @param name
+ * @param email
+ * @param mobileNumber
+ */
+public record AccountsMsgDto(Long accountNumber,
+                             String name,
+                             String email,
+                             String mobileNumber) {
+}
